779b31b3bb60fccc268e57d28aa128005a8e415e
BolatMukashev <ya.ne.angel.kimi@gmail.com>
2026-04-30 11:16:28 +0500

    тестовый запуск в webhook

--- config.py
+++ config.py
@@ -6,8 +6,6 @@ config = dotenv_values(".env")
 BOT_TOKEN = os.environ.get("BOT_TOKEN") or config.get('BOT_TOKEN')
 TEST_BOT_TOKEN = os.environ.get("TEST_BOT_TOKEN") or config.get('TEST_BOT_TOKEN')
 
-TEST_CHAT_ID = os.environ.get("TEST_CHAT_ID") or config.get('TEST_CHAT_ID')
-
 ADMIN_ID = int(os.environ.get("ADMIN_ID") or config.get('ADMIN_ID'))
 
 YDB_TOKEN = os.environ.get("YDB_TOKEN") or config.get('YDB_TOKEN')
